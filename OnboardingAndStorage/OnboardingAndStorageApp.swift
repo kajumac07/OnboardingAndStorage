2cc3cf460780720caa85c00f920d4a3ebf4b8479
kajumac07 <kajumac13@gmail.com>
2025-09-27 10:24:41 +0530

    Initial Commit

---
+++ OnboardingAndStorage/OnboardingAndStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingAndStorageApp.swift
+//  OnboardingAndStorage
+//
+//  Created by Atul Tiwari on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingAndStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
